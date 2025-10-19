d1500fee8a9c6f84ca05d4453aa3c1c9616e0a4b
Muhammed Navas <navaspavil@gmail.com>
2025-10-19 08:31:40 +0530

    Add stack navigation package and it's dependencies

--- android/app/src/main/java/com/reactnativeclisample/MainActivity.kt
+++ android/app/src/main/java/com/reactnativeclisample/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.reactnativeclisample
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory;
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +21,10 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState);
+  }
+
 }
